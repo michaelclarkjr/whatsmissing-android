3ddae3d5bbeb6a9adae5f68f501afe1738ecdd9e
Clark, Michael L <michael.clark2@kroger.com>
2018-07-24 13:02:27 -0400

    init

---
+++ app/src/main/java/com/michaelclark/whatsmissing/WhatsMissingService.java
@@ -0,0 +1,12 @@
+package com.michaelclark.whatsmissing;
+
+public final class WhatsMissingService {
+    public static final String API_URL = "https://something something azure"; //TODO get from team
+
+//    // TODO Define Request/Response Body
+//    public interface SendImage {
+//        @Multipart
+//        @POST("/something") //TODO get from team
+//        Call<ResponseBody> sendImageData(@Part MultipartBody.Part image, @Part RequestBody someData);
+//    }
+}
